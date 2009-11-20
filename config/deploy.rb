bbcff71d2e141cee8404f2bbcee1161ede9c40fa
Michael Thomas <mdthomas22@gmail.com>
2009-11-20 12:44:45 -0500

    Added cap files

---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+set :rails_env, "production"
+
+set :application, "michaelthethomas"
+set :repository,  "git://github.com/mthomas22/michaelthethomas.git"
+set :user, "root"
+set :password, "thomas3l5feX"
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_via, :copy
+set :deploy_to, "/var/www/apps/#{application}"
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+default_run_options[:pty] = true
+set :scm, "git"
+set :scm_passphrase, "password"
+set :user, "mthomas22"
+
+set :branch, "master"
+set :git_shallow_clone, 1
+
+
+# role :app, "your app-server here"
+role :web, "michaelthethomas.com"
+# role :db,  "your db-server here", :primary => true
